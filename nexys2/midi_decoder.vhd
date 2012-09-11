4068e95a49bbcb678212b0a81a5779b9f2d30af4
Kurt Snieckus <ksnieck@gmail.com>
2012-09-10 22:09:00 -0400

    Basic midi input to sawtooth output works!

--- nexys2/midi_decoder.vhd
+++ nexys2/midi_decoder.vhd
@@ -31,19 +31,19 @@ use IEEE.NUMERIC_STD.all;
 --use UNISIM.VComponents.all;
 
 entity midi_decoder is
-    Port ( din : in  STD_LOGIC_VECTOR (7 downto 0);
-           wr : out  STD_LOGIC;
-           nrdy : in  STD_LOGIC;
-           clk : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           midi_chan : in std_logic_vector (3 downto 0);
-           
-           param_cmd : out  STD_LOGIC_VECTOR (7 downto 0);
-           param_val : out  STD_LOGIC_VECTOR (15 downto 0));
+	Port ( din : in  STD_LOGIC_VECTOR (7 downto 0);
+		wr : out  STD_LOGIC;
+		nrdy : in  STD_LOGIC;
+		clk : in  STD_LOGIC;
+		reset : in  STD_LOGIC;
+		midi_chan : in std_logic_vector (3 downto 0);
+		midiclk : out std_logic;
+
+		param_cmd : out  STD_LOGIC_VECTOR (7 downto 0);
+		param_val : out  STD_LOGIC_VECTOR (15 downto 0));
 end midi_decoder;
 
 architecture RTL of midi_decoder is
-
 	-- Type definitions
 	type recv_state_type is (idle, data, done);
 
@@ -57,7 +57,16 @@ architecture RTL of midi_decoder is
 	signal param_cmd_d :  STD_LOGIC_VECTOR (7 downto 0);
 	signal param_val_d : STD_LOGIC_VECTOR (15 downto 0);
 
+	signal wr_d : std_logic := '0';
+	signal midiclk_d : std_logic := '0';
+
 begin
+	-- Because this process is run on every clock edge, and will always read out the next byte from the
+	-- serial fifo, we try just leaving the wr flag high all the time
+	wr_d <= '1';
+	wr <= '1';
+
+	midiclk <= midiclk_d;
 
 	-- Midi byte collection process
 	process(clk)
@@ -70,13 +79,15 @@ begin
 			
 			case recv_state is
 				when idle =>		-- Read incomming byte, and determine if it's a status byte
-					-- First of all, everything can be reset (a reset state if you will)
+					-- First of all, everything can be reset 
 					param_cmd <= (others => '1');		-- address is NOP when all high
 					param_val <= (others => '0');
 					
 					-- Output signal prelatch hold things
 					param_cmd_d <= (others => '1');		-- address is NOP when all high
 					param_val_d <= (others => '0');
+
+					midiclk_d <= '0';
 			
 					-- reset internal signals
 					byte_cnt <= 0;
@@ -88,7 +99,7 @@ begin
 						-- Set command storage buffer based on midi status message
 						param_cmd_d <= (others => '0'); -- Set all zeros to start
 						param_cmd_d(2 downto 0) <= din(6 downto 4); -- lowest three equal address
-						
+
 						-- Change the state to data because that's what comes after a status byte
 						recv_state <= data;
 						
@@ -98,28 +109,34 @@ begin
 								byte_cnt <= 2; -- 2 Bytes for a note off
 							when "001" => -- Note on
 								byte_cnt <= 2; -- 2 bytes for a note on
+							when "111" => -- Midi Time clock (output for testing and shits)
+								if din(3 downto 0) = "1000" then
+									midiclk_d <= not midiclk_d;
+								end if;
 							when others => -- Midi message we are not dealing with
 								recv_state <= idle; -- Reset midi controller and wait for the next status byte
 						end case;
+
 					else
 						recv_state <= idle; -- no status byte and we are not expecting data so continue idle
 					end if;
 					
 				when data =>		-- use midi data messages as required by midi spec
-					case param_cmd_d is
-						when "00000000"|"00000001" => -- Note off or on
-							if byte_cnt = 2 then -- first byte is note number
-								param_val_d(6 downto 0) <= din(6 downto 0);
-								byte_cnt <= 1;
-							elsif byte_cnt = 1 then -- second byte is velocity
-								param_val_d(13 downto 7) <= din(6 downto 0);
-								byte_cnt <= 0;
-								recv_state <= done; -- Last byte, latch data and look for next status byte
-							end if;
-						when others => -- catch things that we havn't written yet
-							recv_state <= idle; -- and then don't do something stupid
-					end case;
-					
+					if (nrdy = '0') and (din(7) = '0') then  -- a data byte is less than 128
+						case param_cmd_d is
+							when "00000000"|"00000001" => -- Note off or on
+								if byte_cnt = 2 then -- first byte is note number
+									param_val_d(6 downto 0) <= din(6 downto 0);
+									byte_cnt <= 1;
+								elsif byte_cnt = 1 then -- second byte is velocity
+									param_val_d(13 downto 7) <= din(6 downto 0);
+									byte_cnt <= 0;
+									recv_state <= done; -- Last byte, latch data and look for next status byte
+								end if;
+							when others => -- catch things that we havn't written yet
+								recv_state <= idle; -- and then don't do something stupid
+						end case;
+					end if;
 				when done =>		-- Last midi data message recieved, latch data onto param bus
 					-- Latch _d's to outputs so that the connected devices respond to midi message
 					param_cmd <= param_cmd_d;
--- nexys2/test_osc.vhd
+++ nexys2/test_osc.vhd
@@ -40,30 +40,43 @@ end test_osc;
 
 architecture RTL of test_osc is
 	
+	signal note_val	: std_logic_vector(6 downto 0) := (others => '0');
+	signal note_vel	: std_logic_vector(6 downto 0) := (others => '0');
+	signal fcw 			: unsigned (15 downto 0) := (others => '0');
 	
-	type fcwlut_type is array ( 0 to 127 ) of unsigned (15 downto 0);
-	signal fcwlut : fcwlut_type;
-
-	signal note_val	: std_logic_vector(6 downto 0);
-	signal note_vel	: std_logic_vector(6 downto 0);
-	signal fcw 			: unsigned (15 downto 0);
-	
-	signal phase		: unsigned (15 downto 0);
-	
-begin
-
-	-- Generate the LUT for the Freqeuncy Control Word
+	signal phase		: unsigned (15 downto 0) := (others => '0');
+		
+	-- The LUT for the Freqeuncy Control Word
 	-- FCW (Delta F) = Fout*2^N / Fclock
 	-- midi note number to frequency conversion:
 	-- f=(2^((m−69)/12))*(440 Hz).
 	-- So to get the increment value from the midi note
 	-- FCW = 2^((m-69)/12) * 440 Hz * 2^N / Fclock
 	-- Using N = 16 gives us a frequency step size of 0.7324 hz which is nice
+
+ 
+
+	-- Generated with FreeMAT (MATLAB Equiv)
+	-- m = linspace(0,127,128)
+	-- fcwlut = (2.^((m-69)/12) * 440 * 65536 / (48000));
+	-- printf('X"%04X",',fcwlut)
+
+	type fcwlut_type is array ( 0 to 127 ) of unsigned (15 downto 0);
+	signal fcwlut : fcwlut_type := (X"000B",X"000B",X"000C",X"000D",X"000E",X"000E",X"000F",X"0010",X"0011",X"0012",X"0013",X"0015",X"0016",X"0017",X"0019",X"001A",X"001C",X"001D",X"001F",X"0021",X"0023",X"0025",X"0027",X"002A",X"002C",X"002F",X"0032",X"0035",X"0038",X"003B",X"003F",X"0042",X"0046",X"004B",X"004F",X"0054",X"0059",X"005E",X"0064",X"006A",X"0070",X"0077",X"007E",X"0085",X"008D",X"0096",X"009F",X"00A8",X"00B2",X"00BD",X"00C8",X"00D4",X"00E1",X"00EE",X"00FC",X"010B",X"011B",X"012C",X"013E",X"0151",X"0165",X"017A",X"0190",X"01A8",X"01C2",X"01DC",X"01F9",X"0217",X"0237",X"0258",X"027C",X"02A2",X"02CA",X"02F4",X"0321",X"0351",X"0384",X"03B9",X"03F2",X"042E",X"046E",X"04B1",X"04F8",X"0544",X"0594",X"05E9",X"0643",X"06A3",X"0708",X"0773",X"07E4",X"085C",X"08DC",X"0962",X"09F1",X"0A89",X"0B29",X"0BD3",X"0C87",X"0D46",X"0E10",X"0EE6",X"0FC9",X"10B9",X"11B8",X"12C5",X"13E3",X"1512",X"1653",X"17A7",X"190F",X"1A8C",X"1C20",X"1DCD",X"1F92",X"2173",X"2370",X"258B",X"27C7",X"2A25",X"2CA6",X"2F4E",X"321E",X"3519",X"3841",X"3B9A",X"3F25",X"42E6");
+
+
+	-- This doesn't work probably due to bad math support in ieee packages? i dunno, just used a table instead.
+	--gen_fcwlut:
+	--for mf in 0 to 127 generate
+	--	fcwlut(mf) <= to_unsigned((integer(2**(real((mf-69)/12))) * 440 * 65536 / 48000), 16);
+	--end generate gen_fcwlut;
+
+	-- Oscillator enable flag
+	signal en : std_logic := '0' ;
+
+begin
 	
-	gen_fcwlut:
-	for mf in 0 to 126 generate
-		fcwlut(mf) <= to_unsigned((2**(abs((mf-69)/12)) * 440 * 65536 / (48000)), 16);
-	end generate gen_fcwlut;
+	outen <= en;
 
 	-- Data readin
 	process(clk)
@@ -72,8 +85,9 @@ begin
 			if param_cmd = "00000000" and param_val(6 downto 0) = note_val then
 				-- note off	
 				note_vel <= (others => '0');
-				outen <= '0';
-			elsif param_cmd = "00000001" and note_vel = "0000000" then
+				en <= '0';
+
+			elsif param_cmd = "00000001" then
 				-- we are not playing a note and we got a note on
 				note_val <= param_val(6 downto 0);
 				note_vel <= param_val(13 downto 7);
@@ -81,7 +95,7 @@ begin
 				-- get the FCW from the LUT
  				fcw <= fcwlut(to_integer(unsigned(param_val(6 downto 0))));
 				
-				outen <= '1';
+				en <= '1';
 				
 			end if;
 			
@@ -94,10 +108,14 @@ begin
 	process(samp_clk)
 	begin
 		if rising_edge(samp_clk) then 
-			-- increment accumulator
-			phase <= phase + fcw;
-			-- Output sawtooth, this has bad rounding but i don't care rn
-			dout <= std_logic_vector(phase);
+			if en = '1' then
+				-- increment accumulator
+				phase <= phase + fcw;
+				-- Output sawtooth, this has bad rounding but i don't care rn
+				dout <= std_logic_vector(phase);
+			else 
+				dout <= "1000000000000000"; 
+			end if;
 		end if;
 	end process;
 end RTL;
